9343523f76080e8c634ee9d52c312032ddb12a8f
syedma <ahrissyed@gmail.com>
2023-05-03 21:49:04 +0500

    swagger paths fixed

--- swagger.js
+++ swagger.js
@@ -1,7 +1,8 @@
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
-const endpointsFiles = ['./routes/userRoutes.js','./routes/intersectionRoutes.js','./routes/signalRoutes.js','./index.js']
+// const endpointsFiles = ['./routes/userRoutes.js','./routes/intersectionRoutes.js','./routes/signalRoutes.js','./index.js']
+const endpointsFiles = ['./index.js']
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
     require('./index.js')

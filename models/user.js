b0157d3c3035cc84cceed43fd2621874e144bdc9
Hamza Salman <hamza.salmann9@gmail.com>
2023-05-05 20:33:00 +0500

    Email formatter added to login and register

--- models/user.js
+++ models/user.js
@@ -10,31 +10,29 @@ const userSchema = new mongoose.Schema({
         type: String,
         unique:true,
         validate: {
-            validator: async function (email) {
-                const user = await this.constructor.findOne({ email });
-                if (user) {
-                    if (this.id === user.id) {
-                        return true;
-                    }
-                    return false;
-                }
-                return true;
-            },
-            message: props => `${props.value} already exists!`
-        },
-        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      validator: async function (email) {
+        const user = await this.constructor.findOne({ email });
+        if (user) {
+          if (this.id === user.id) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      },
+      message: props => `${props.value} already exists!`
+    }
     },
     password: {
         type: String,
         minlength: 6,
         required: true,
-    },
+      },
     role: {
         type: String,
-        enum: ['Basic', 'Admin'],
-        default: 'Basic',
+        default: "Basic",
         required: true,
-    },
-});
+      },
+})
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)

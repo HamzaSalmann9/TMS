bf6d69cf362a4a6c97b409c536df4ff767d5bc02
syedma <ahrissyed@gmail.com>
2023-05-23 17:50:13 +0500

    master_key code

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,17 +1,28 @@
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-  const token = req.cookies.jwt;
-
-  if (!token) return res.status(401).send('Access Denied');
-
-  try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    res.locals.token = token; // pass token to res.locals object
-    next();
-  } catch (error) {
-    res.status(400).send('Invalid Token');
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+      if (err) {
+        res.status(403).json("Token is not valid.");
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    const token = req.cookies.jwt;
+    if (!token) return res.status(401).send('Access Denied');
+    try {
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = verified;
+      res.locals.token = token; // pass token to res.locals object
+      next();
+    } catch (error) {
+      res.status(400).send('Invalid Token');
+    }
   }
 }
 
